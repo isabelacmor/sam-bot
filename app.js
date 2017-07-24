2be14a4cf32f6960e7e4d36ca29d3745054f3965
Isabella Moreira <isabella.moreira@savvysuit.com>
2017-07-24 13:58:02 -0700

    deploy just the bot

---
+++ app.js
@@ -0,0 +1,23 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var secret = require('./secret.json');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: secret.bot.app_id,
+    appPassword: secret.bot.app_password
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
