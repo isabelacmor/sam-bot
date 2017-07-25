cc2fcd0b8c1beb9428929eedf8366dcb796b6a89
Isabella Moreira <isabella.moreira@savvysuit.com>
2017-07-24 20:06:10 -0700

    add backchannel

--- app.js
+++ app.js
@@ -122,3 +122,30 @@ bot.dialog('startMeditation', function (session, args, next) {
 .triggerAction({
     matches: /^start meditation$/i,
 });
+
+//Bot listening for inbound backchannel events - in this case it only listens for events named "buttonClicked"
+bot.on("event", function (event) {
+    var msg = new builder.Message().address(event.address);
+    msg.textLocale("en-us");
+    if (event.name === "buttonClicked") {
+        msg.text("I see that you just pushed that button");
+    }
+    bot.send(msg);
+})
+
+//Basic root dialog which takes an inputted color and sends a changeBackground event. No NLP, regex, validation here - just grabs input and sends it back as an event.
+bot.dialog('changebg', [
+    function (session) {
+        var reply = createEvent("changeBackground", session.message.text, session.message.address);
+        session.endDialog(reply);
+    }
+]);
+
+//Creates a backchannel event
+const createEvent = (eventName, value, address) => {
+    var msg = new builder.Message().address(address);
+    msg.data.type = "event";
+    msg.data.name = eventName;
+    msg.data.value = value;
+    return msg;
+}
