23cdd0c3419e365a65133d6e06a4385a49d34de0
Isabella Moreira <isabella.moreira@savvysuit.com>
2017-07-25 14:51:04 -0700

    added custom looping dialog

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var secret = require('./secret.json');
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -23,41 +31,59 @@ var username_key = 'UserName';
 var userWelcomed_key = 'UserWelcomed';
 var currentFeeling_key = "CurrentFeeling";
 
-//var feelingsArray = [{'feeling':'Tired', 'img': 'https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__16-20.png'}, {'feeling':'Frustrated', 'img': 'https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__16-20.png'}, {'feeling':'Sad', 'img': 'https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__16-20.png'}, {'feeling':'Anxious', 'img': 'https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__16-20.png'}];
-var feelingsArray = ["Happy", 'Fine', 'Okay', 'Depressed', 'Sad'];
-
-// Map choices into actions
+// Feelings definitions
+var feelingsArray = ["Sad", 'Lonely', 'Anxious'];
 var msg = new builder.Message()
   .addAttachment({
       text: '',
-      thumbnailUrl: 'http://imgur.com/HrRp7zd.png',
-      actions: [ { title: 'Happy', message: 'Happy' }]
+      thumbnailUrl: 'http://i.imgur.com/a4RlGrv.png',
+      actions: [ { title: 'Sad', message: 'Sad' }]
    })
   .addAttachment({
       text: '',
       thumbnailUrl: 'http://imgur.com/lyF5JK5.png',
-      actions: [ { title: 'Fine', message: 'Fine' }]
+      actions: [ { title: 'Lonely', message: 'Lonely' }]
    })
   .addAttachment({
       text: '',
       thumbnailUrl: 'http://imgur.com/caVlZJZ.png',
-      actions: [ { title: 'Okay', message: 'Okay' }]
-  })
-  .addAttachment({
-      text: '',
-      thumbnailUrl: 'http://imgur.com/ijPsb3q.png',
-      actions: [ { title: 'Depressed', message: 'Depressed' }]
-   })
-   .addAttachment({
-       text: '',
-       thumbnailUrl: 'http://i.imgur.com/a4RlGrv.png',
-       actions: [ { title: 'Sad', message: 'Sad' }]
-    });
+      actions: [ { title: 'Anxious', message: 'Anxious' }]
+  });
 
-// This is a dinner reservation bot that uses multiple dialogs to prompt users for input.
+// Phrases
+var phrases = {
+  validating: [
+    "Of course you're feeling this way. It must be a difficult situation.",
+    "It's okay to feel this way and it means you're human.",
+    "I can see how difficult this is for you. No one should have to go through that.",
+    "You're not wrong for feeling the way you do and no one blames you for it."
+  ],
+  prompt: [
+    "Do you want to talk about it?",
+    "Would you like to talk about it?",
+    "Do you want to tell me more about what you're feeling?",
+    "Would you like to tell me more?"
+  ],
+  terminating: [
+    "That's okay. I'll be here if you change your mind.",
+    "I know this can be difficult to talk about. If you decide to share more with me, I'll be here.",
+    "I understand. I'll be here for you when you're ready.",
+    "You're not alone in this. I'm here if you need me.",
+    "Take your time. No one is rushing you to feel better. I'll be where when you need me."
+  ],
+  action: [
+    "Would you like to watch a cute video to take your mind off things?",
+    "Would you like me to play an upbeat song?",
+    "Would you like to start a short meditation session?",
+    "Going for a walk outside might help you clear your mind for a bit.",
+    "Eating a sweet snack will give your brain a good-feeling boost."
+  ]
+};
+
+// Main bot flow
 var bot = new builder.UniversalBot(connector, [
+    // Ask for feeling
     function (session) {
-        //session.send("Welcome to the dinner reservation.");
         // is user's name set?
         var userName = session.userData[username_key];
         if (!userName) {
@@ -71,29 +97,25 @@ var bot = new builder.UniversalBot(connector, [
         }
 
         session.beginDialog('askForFeeling');
-    },
+    },  // Initial validation + prompt response
     function (session, results) {
         session.userData[currentFeeling_key] = results.response.entity.toLowerCase();
-        switch (results.response.entity) {
-            case 'Happy':
-                session.beginDialog('processFeeling');
-                break;
-            case 'Fine':
-                session.beginDialog('processFeeling');
-                break;
-            case 'Okay':
-                session.beginDialog('processFeeling');
-                break;
-            case 'Depressed':
-                session.beginDialog('processFeeling');
-                break;
-            case 'Sad':
-                session.beginDialog('processFeeling');
-                break;
-            default:
-                session.send('invalid choice');
-                break;
-        }
+        session.send(phrases.validating[getRandomInt(0, phrases.validating.length-1)]);
+        session.beginDialog('promptDiscussion');
+        // switch (results.response.entity) {
+        //     case 'Sad':
+        //         session.beginDialog('processFeeling');
+        //         break;
+        //     case 'Lonely':
+        //         session.beginDialog('processFeeling');
+        //         break;
+        //     case 'Anxious':
+        //         session.beginDialog('processFeeling');
+        //         break;
+        //     default:
+        //         session.send('invalid choice');
+        //         break;
+        //}
         //session.beginDialog('processFeeling');
     },
     function (session, results) {
@@ -119,21 +141,41 @@ bot.dialog('greet', new builder.SimpleDialog(function (session, results) {
 bot.dialog('askForFeeling', [
     function (session) {
         //builder.Prompts.choice(session, msg, feelingsArray);
-        builder.Prompts.choice(session, 'How are you feeling right now?', feelingsMessage, {listStyle: builder.ListStyle.button});
+        builder.Prompts.choice(session, 'How are you feeling right now?', feelingsArray, {listStyle: builder.ListStyle.button});
     },
     function (session, results) {
         session.endDialogWithResult(results);
     }
 ]);
 
-// Dialog to process the user's feelings and offer suggestions
-bot.dialog('processFeeling', [
+// Dialog to ask the user if they want to talk about their feeling
+bot.dialog('promptDiscussion', [
     function (session) {
-        var prompt = "I understand you are feeling " + session.userData[currentFeeling_key] + ". Do you want to listen to music?";
-        builder.Prompts.text(session, prompt);
+        var prompt = phrases.prompt[getRandomInt(0, phrases.prompt.length-1)];
+        builder.Prompts.choice(session, prompt, "yes|no", {listStyle: builder.ListStyle.button});
     },
     function (session, results) {
-        session.endDialogWithResult(results);
+        session.send(results.response.entity);
+        switch (results.response.entity) {
+          case 'yes':
+            session.beginDialog('doDiscussion');
+            break;
+          case 'no':
+            // Continue to prompt for suggestion
+            session.endDialogWithResult(results);
+            break;
+        }
+    }
+]);
+
+// Dialog to ask the user to talk more about their feelings
+bot.dialog('doDiscussion', [
+    function (session) {
+        builder.Prompts.text(session, "I'm listening. Go ahead.");
+    },
+    function (session, results) {
+        session.send(phrases.validating[getRandomInt(0, phrases.validating.length-1)]);
+        session.beginDialog('promptDiscussion');
     }
 ]);
 
